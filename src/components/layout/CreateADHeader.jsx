b9021178f669bd83aa887f03be2eeb5186cc7ab8
devlsn96 <bansw96@gmail.com>
2026-01-27 12:02:51 +0900

    chore : 광고 생성 UI  헤더 디자인 수정 보완

--- src/components/layout/CreateADHeader.jsx
+++ src/components/layout/CreateADHeader.jsx
@@ -1,18 +1,28 @@
 import { Link } from "react-router-dom";
+import leftArrow from "@/assets/image/left-arrow.png"
+import Container from "../common/Container";
 
 export function CreateADHeader() {
-
   return (
     <>
-      <header className="w-full bg-white">
-        <div className="mr-auto ml-auto flex h-16 max-w-7xl px-6 items-center justify-center">
-          <Link to={"./dashboard"}>대시보드로</Link>
-          <div className="m-8 mr-220">
-            <h1 className="text-2xl font-bold">AI 광고 생성</h1>
-            <p className="text-sm text-gray-500 mt-1">Step 1 / 5 · 정보 입력</p>
+      <header className="w-full bg-white border-b grayscale-25">
+        <Container className="flex h-14 items-center justify-between text-xs">
+          <Link
+            to={"./dashboard"}
+            className="w-20 flex items-center gap-2 font-bold text-gray-600 hover:text-gray-800"
+          >
+            <img src={leftArrow} className="h-3 opacity-30"></img>대시보드
+          </Link>
+          <div className="mr-200">
+            <h1 className="font-bold text-xl">AI 광고 생성</h1>
           </div>
-          <Link to={".."}>메인으로</Link>
-        </div>
+          <Link
+            to={".."}
+            className="rounded-md border border-gray-200 px-3 py-1 text-xs font-semibold text-gray-600"
+          >
+            메인
+          </Link>
+        </Container>
       </header>
     </>
   );
