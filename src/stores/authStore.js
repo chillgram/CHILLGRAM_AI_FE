87e903680444b7f1a234df9697f30199ca901eed
taemin208 <tae6633@naver.com>
2026-01-29 11:48:58 +0900

    fix(auth): 로그인 bootstrapped 누락

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -3,6 +3,7 @@ import { create } from "zustand";
 export const useAuthStore = create((set) => ({
   accessToken: null,
   isAuthenticated: false,
+  bootstrapped: false,
 
   login: (accessToken) =>
     set({
@@ -15,4 +16,6 @@ export const useAuthStore = create((set) => ({
       accessToken: null,
       isAuthenticated: false,
     }),
+
+  setBootstrapped: (value) => set({ bootstrapped: value }),
 }));
